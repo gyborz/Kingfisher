6c578a47b3bb6f039ac1e0c638cf7506fbbb0d47
gyborz <borzgy95@gmail.com>
2021-09-27 09:34:34 +0200

    bump package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Kingfisher",
-    platforms: [.iOS(.v12), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
+    platforms: [.iOS(.v13), .macOS(.v10_14), .tvOS(.v12), .watchOS(.v5)],
     products: [
         .library(name: "Kingfisher", targets: ["Kingfisher"])
     ],
